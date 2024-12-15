1fc62b9d1d683cbdcde355ba21a151d15031012f
Nguyen Van Tuan <nguyentuan277tq@gmail.com>
2024-12-15 17:27:23 +0700

    Day 1 - chapter 1 to 5

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,9 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from "next/font/google";
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ["latin"] });
 
 export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
 });
